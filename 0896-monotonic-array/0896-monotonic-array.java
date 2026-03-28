a4d8d75ba39e1b6edd7dfa6cd9f7d8953d11856f
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-28 17:16:44 +0530

    Time: 3 ms (19.04%), Space: 85.2 MB (73.69%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,20 +1,32 @@
+// class Solution {
+//     public boolean isMonotonic(int[] nums) {
+//         int n = nums.length;
+//         if (nums[0] <= nums[n - 1]) {
+//             for (int i = 0; i < n - 1; i++) {
+//                 if (nums[i] > nums[i + 1]) {
+//                     return false;
+//                 }
+//             }
+//         } else {
+//             for (int i = 0; i < n - 1; i++) {
+//                 if (nums[i] < nums[i + 1]) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int n = nums.length;
-        if(nums[0] <= nums[n-1]){
-            for(int i = 0; i < n-1; i++){
-                if(nums[i] > nums[i+1]){
-                    return false;
-                }
-            }
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] < nums[i+1]) decreasing = false;
+            if(nums[i] > nums[i+1]) increasing = false;
         }
-        else{
-            for(int i = 0; i < n-1; i++){
-                if(nums[i] < nums[i+1]){
-                    return false;
-                }
-            }
-        }
-        return true;
+
+        return increasing || decreasing;
     }
 }
