eb0bfaaee1fd692d600bc61715c2568e145626b5
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-29 19:58:04 +0530

    Time: 157 ms (100%), Space: 48.1 MB (50%) - LeetHub

---
+++ 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minCost(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int max = 1024;
+        boolean[][][] d = new boolean[m][n][max];
+        d[0][0][grid[0][0]] = true;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < max; k++) {
+                    if (d[i][j][k]) {
+                        if (i + 1 < m) {
+                            d[i + 1][j][k ^ grid[i + 1][j]] = true;
+                        }
+                        if (j + 1 < n) {
+                            d[i][j + 1][k ^ grid[i][j + 1]] = true;
+                        }
+                    }
+                }
+            }
+        }
+        for( int k = 0; k < max; k++){
+            if(d[m-1][n-1][k]){
+                return k;
+            }
+        }
+        return 0;
+    }
+
+}
