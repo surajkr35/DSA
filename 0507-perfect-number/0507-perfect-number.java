43dccfdb27bcab72ee980432606dcacdc885de72
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-31 01:07:56 +0530

    Time: 0 ms (100%), Space: 42.2 MB (55.59%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-
+        // for a number to be perfect number
+        // n = 2^(p−1) × (2^p − 1)  
+        // p must be prime number  ---- 2 , 3 , 5 , 7 ....
+        
         int[] primes = {2, 3, 5, 7, 13};
         for (int p : primes) {
             int m = (int)Math.pow(2, p) - 1;
