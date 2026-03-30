cb9df60250daafd7d80e5a2c011688482824712b
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-31 00:47:44 +0530

    Time: 1831 ms (64.46%), Space: 42 MB (86.75%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,12 +1,24 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for (int i = 1; i <= num / 2; i++) {
+        if (num <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
+                return checkForNonPrime(num);
+            }
+        }
+        return false;
+
+    }
+
+    private boolean checkForNonPrime(int x) {
+        int sum = 0;
+        for (int i = 1; i <= x / 2; i++) {
+            if (x % i == 0) {
                 sum = sum + i;
             }
         }
-        if (sum == num) {
+        if (sum == x) {
             return true;
         }
         return false;
