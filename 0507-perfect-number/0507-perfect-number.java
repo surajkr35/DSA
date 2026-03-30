b0a9e17f90ed92cebe9035024633fb016fc8ae21
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-31 01:07:20 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.59%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int[] primes = {2, 3, 5, 7, 13};
 
+        int[] primes = {2, 3, 5, 7, 13};
         for (int p : primes) {
-            int mersenne = (int)Math.pow(2, p) - 1;
-            int perfect = (int)Math.pow(2, p - 1) * mersenne;
+            int m = (int)Math.pow(2, p) - 1;
+            int perfect = (int)Math.pow(2, p - 1) * m;
 
             if (perfect == num) {
                 return true;
