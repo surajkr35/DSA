e2e8dd92c92580580aa3e60f5c41ca4c0cd045d0
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-30 23:52:53 +0530

    Time: 0 ms (100%), Space: 42.9 MB (55.63%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -3,3 +3,22 @@ class Solution {
         return Integer.toString(num, 7);
     }
 }
+// class Solution {
+//     public String convertToBase7(int num) {
+//         if (num == 0) return "0";
+
+//         boolean negative = num < 0;
+//         num = Math.abs(num);
+//         StringBuilder sb = new StringBuilder();
+
+//         while (num > 0) {
+//             int rem = num % 7;
+//             sb.append(rem);   // append remainder
+//             num /= 7;         // divide by 7
+//         }
+
+//         if (negative) sb.append('-');
+
+//         return sb.reverse().toString();
+//     }
+// }
