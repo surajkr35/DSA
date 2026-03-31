3a21d5ac05128a3ce0e8f50d450c43c6e141031c
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-31 12:57:28 +0530

    Time: 0 ms (100%), Space: 42.1 MB (43.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,28 @@
+// Using DP, Iterative with 2 Variables.  [ O(n) Time Complexity ]
+
 class Solution {
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        else return fib(n-1) + fib(n-2);
+        if (n <= 1) return n;
+
+        int prev2 = 0; // F(0)
+        int prev1 = 1; // F(1)
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 }
+
+// Using Recursion
+
+// class Solution {
+//     public int fib(int n) {
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
+//         else return fib(n-1) + fib(n-2);     [ O(2^n) Time Complexity ]
+//     }
+// }
