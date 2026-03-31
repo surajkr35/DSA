14e7fa29c009306a9edc823f08a88765ffae269b
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-31 21:52:23 +0530

    Time: 8 ms (99.22%), Space: 48.2 MB (88.65%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] fairCandySwap(int[] alice, int[] bob) {
+        int sumA = 0, sumB = 0;
+
+        for (int a : alice) sumA += a;
+        for (int b : bob) sumB += b;
+
+        int diff = (sumB - sumA) / 2;
+
+        Set<Integer> setB = new HashSet<>();
+        for (int b : bob) setB.add(b);
+
+        for (int a : alice) {
+            if (setB.contains(a + diff)) {
+                return new int[]{a, a + diff};
+            }
+        }
+
+        return new int[]{};
+    }
+}
