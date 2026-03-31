34243ca762007f4c4a1f5f947cd769f80449a7e2
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-03-31 14:24:18 +0530

    Time: 4 ms (99.74%), Space: 47.7 MB (79.13%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -10,8 +10,7 @@ class Solution {
             }
         }
         return true;
-    }
-    
+    }  
     private boolean isPalindrome(String s, int left, int right) {
         while (left < right) {
             if (s.charAt(left++) != s.charAt(right--)) return false;
