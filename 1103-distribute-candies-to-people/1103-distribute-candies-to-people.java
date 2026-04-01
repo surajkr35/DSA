5ca0354e64a34d783743929314e339e22df8729b
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-01 22:25:36 +0530

    Time: 2 ms (90.28%), Space: 42.3 MB (96.64%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int n = num_people;
+        int x = 1, sum = 0, i = 0;
+        int[] res = new int[n];
+
+        while(sum < candies){
+            if(i == n){
+                i = 0;
+            }
+            if(x <= candies-sum){
+                res[i] = res[i] + x;
+                sum = sum + x;
+                i++;
+                x++;
+            }
+            else{
+                res[i] = res[i] + candies - sum;
+                sum = sum + res[i];
+            }
+        }
+        return res;
+    }
+}
