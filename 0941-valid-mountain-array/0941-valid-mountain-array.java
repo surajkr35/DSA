73359546af1a7ac765b6dbbae1a3f97cbb144ada
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-01 19:40:40 +0530

    Time: 1 ms (99.98%), Space: 47.4 MB (66.33%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean validMountainArray(int[] arr) {
         int n = arr.length;
         if(n < 3) return false;
-
         int i = 0;
+
         while(i + 1 < n && arr[i] < arr[i+1]) i++;
 
         if(i == 0 || i == n - 1) return false;
