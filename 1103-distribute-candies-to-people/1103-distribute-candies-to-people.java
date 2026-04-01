a5d43858de7b313d28f525409ebb4f933d950c4c
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-01 22:40:45 +0530

    Time: 2 ms (90.28%), Space: 42.6 MB (65.05%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.java
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.java
@@ -22,3 +22,20 @@ class Solution {
         return res;
     }
 }
+// class Solution {
+//     public int[] distributeCandies(int candies, int num_people) {
+//         int[] res = new int[num_people];
+        
+//         int give = 1;  
+//         int i = 0;     
+        
+//         while (candies > 0) {
+//             res[i % num_people] += Math.min(give, candies);
+//             candies -= give;
+//             give++;
+//             i++;
+//         }
+        
+//         return res;
+//     }
+// }
