3640e451bcb0efa1f4716285d43dd5180a57e1ad
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-12 03:02:00 +0530

    Time: 0 ms (100%), Space: 45.6 MB (84.25%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
         boolean present = true;
-        
         while(present){
             if(check(nums, original)){
                 original *= 2;
