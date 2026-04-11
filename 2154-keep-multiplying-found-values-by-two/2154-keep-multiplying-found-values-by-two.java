06d901d8eaf2cb8e0a914161ada859d517d762f2
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-12 02:47:37 +0530

    Time: 3 ms (67.15%), Space: 46.3 MB (43.19%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -5,11 +5,9 @@ class Solution {
         for(int num : nums){
             set.add(num);
         }
-
         while(set.contains(original)){
             original *= 2;
         }
-
         return original;
     }
 }
