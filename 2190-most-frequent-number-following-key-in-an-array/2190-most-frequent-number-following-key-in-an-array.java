59f9c6a303ad2b013199671b444ae15c34f96efd
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-12 22:44:19 +0530

    Time: 3 ms (65.47%), Space: 46.5 MB (44.63%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -1,24 +1,42 @@
 class Solution {
     public int mostFrequent(int[] nums, int key) {
-        int n = nums.length, var = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         int maxCount = 0;
+        int result = 0;
 
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] == key){
-                int target = nums[i + 1];
-                int count = 0;
-                
-                for(int j = 0; j < n - 1; j++){
-                    if(nums[j] == key && nums[j + 1] == target){
-                        count++;
-                    }
-                }
-                if(count > maxCount){
-                    maxCount = count;
-                    var = target;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == key) {
+                int next = nums[i + 1];
+                map.put(next, map.getOrDefault(next, 0) + 1);
+
+                if (map.get(next) > maxCount) {
+                    maxCount = map.get(next);
+                    result = next;
                 }
             }
         }
-        return var;
+
+        return result;
     }
 }
+// class Solution {
+//     public int mostFrequent(int[] nums, int key) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int maxCount = 0;
+//         int result = 0;
+
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             if (nums[i] == key) {
+//                 int next = nums[i + 1];
+//                 map.put(next, map.getOrDefault(next, 0) + 1);
+
+//                 if (map.get(next) > maxCount) {
+//                     maxCount = map.get(next);
+//                     result = next;
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// }
