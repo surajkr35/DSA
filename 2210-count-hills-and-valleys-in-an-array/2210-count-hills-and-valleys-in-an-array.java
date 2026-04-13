dfc68967b3df66ed2cb7cd0adccc217b8723df25
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-13 22:24:30 +0530

    Time: 0 ms (100%), Space: 43.6 MB (9.76%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,23 +1,53 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int n = nums.length, count = 0;
+        int count = 0;
 
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] == nums[i - 1]) continue;
+        int prev = nums[0];
 
-            int left = i - 1, right = i + 1;
+        for (int i = 1; i < nums.length - 1; i++) {
 
-            while (left >= 0 && nums[left] == nums[i]) left--;
-            while (right < n && nums[right] == nums[i]) right++;
+            if (nums[i] == prev) continue;
 
-            if (left >= 0 && right < n) {
-                if ((nums[left] > nums[i] && nums[right] > nums[i]) ||
-                    (nums[left] < nums[i] && nums[right] < nums[i])) {
-                    count++;
-                }
+            int next = i + 1;
+
+            while (next < nums.length && nums[next] == nums[i]) {
+                next++;
             }
+
+            if (next == nums.length) break;
+
+            if ((nums[i] > prev && nums[i] > nums[next]) ||
+                (nums[i] < prev && nums[i] < nums[next])) {
+                count++;
+            }
+
+            prev = nums[i];
         }
 
         return count;
     }
 }
+
+// class Solution {
+//     public int countHillValley(int[] nums) {
+//         int n = nums.length, count = 0;
+
+//         for (int i = 1; i < n - 1; i++) {
+//             if (nums[i] == nums[i - 1]) continue;
+
+//             int left = i - 1, right = i + 1;
+
+//             while (left >= 0 && nums[left] == nums[i]) left--;
+//             while (right < n && nums[right] == nums[i]) right++;
+
+//             if (left >= 0 && right < n) {
+//                 if ((nums[left] > nums[i] && nums[right] > nums[i]) ||
+//                     (nums[left] < nums[i] && nums[right] < nums[i])) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// }
