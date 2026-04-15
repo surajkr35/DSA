8706133f3dedffccefda2de6eea80cd59aebb528
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-15 20:49:28 +0530

    Time: 0 ms (100%), Space: 47.3 MB (7.79%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,17 +1,11 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
-        int leftDist = Integer.MAX_VALUE, rightDist = Integer.MAX_VALUE, minDist = Integer.MAX_VALUE;
+
         for(int i = 0; i < n; i++){
-            if(words[(startIndex + i) % n].equals(target)){
-                rightDist = i;
+            if(words[(startIndex + i) % n].equals(target) || words[(startIndex - i + n) % n].equals(target)){
+                return i;
             }
-            if(words[(startIndex - i + n) % n].equals(target)){
-                leftDist = i;
-            }
-            int prevMin = minDist;
-            minDist = Math.min(rightDist, leftDist);
-            if(prevMin != minDist) return minDist;
         }
         return -1;
     }
