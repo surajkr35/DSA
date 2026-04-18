4928deb4dfaa9e6b6ec6d138f40c9777bd644324
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-18 21:35:14 +0530

    Time: 10 ms (58.16%), Space: 47.8 MB (7.02%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,40 +1,34 @@
 // class Solution {
 //     public String[] sortPeople(String[] names, int[] heights) {
-//         HashMap <Integer, String> map = new HashMap<>();
-//         String[] ans = new String[heights.length];
+//         int n = names.length;
+//         int[][] arr = new int[n][2];
 
-//         for(int i = 0; i < heights.length; i++){
-//             map.put(heights[i], names[i]);
+//         for (int i = 0; i < n; i++) {
+//             arr[i][0] = heights[i];
+//             arr[i][1] = i;
 //         }
-//         Arrays.sort(heights);
-//         int x = 0;
-//         for(int i = heights.length - 1; i >= 0; i--){
-//             ans[x++] = map.get(heights[i]);
+//         Arrays.sort(arr, (a, b) -> b[0] - a[0]);
+//         String[] ans = new String[n];
+
+//         for (int i = 0; i < n; i++) {
+//             ans[i] = names[arr[i][1]];
 //         }
 //         return ans;
 //     }
 // }
-import java.util.*;
-
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        int n = names.length;
+        HashMap <Integer, String> map = new HashMap<>();
+        String[] ans = new String[heights.length];
 
-        int[][] arr = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = heights[i];
-            arr[i][1] = i;
+        for(int i = 0; i < heights.length; i++){
+            map.put(heights[i], names[i]);
         }
-
-        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
-
-        String[] ans = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            ans[i] = names[arr[i][1]];
+        Arrays.sort(heights);
+        int x = 0;
+        for(int i = heights.length - 1; i >= 0; i--){
+            ans[x++] = map.get(heights[i]);
         }
-
         return ans;
     }
 }
