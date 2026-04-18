547873c244301e97f2164e04e3d630d4cedd1b5f
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-18 22:11:57 +0530

    Time: 7 ms (58.64%), Space: 47.1 MB (41.67%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -1,28 +1,43 @@
-import java.util.*;
-
+//   Using HashSet   ~ O(n) Time Complexity
 class Solution {
     public int findMaxK(int[] nums) {
-        Arrays.sort(nums);
-
-        int i = 0, j = nums.length - 1;
+        HashSet <Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
         int ans = -1;
-
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-
-            if (sum == 0) {
-                ans = Math.max(ans, nums[j]);
-                i++;
-                j--;
-            } 
-            else if (sum < 0) {
-                i++;
-            } 
-            else {
-                j--;
+        for(int num : set){
+            if(set.contains(-num)){
+                ans = Math.max(ans,num);
             }
         }
-
         return ans;
     }
 }
+//   Using Sorting & 2-Pointers    ~ O(nlogn) Time Complexity 
+
+// class Solution {
+//     public int findMaxK(int[] nums) {
+//         Arrays.sort(nums);
+
+//         int i = 0, j = nums.length - 1;
+//         int ans = -1;
+
+//         while (i < j) {
+//             int sum = nums[i] + nums[j];
+
+//             if (sum == 0) {
+//                 ans = Math.max(ans, nums[j]);
+//                 i++;
+//                 j--;
+//             } 
+//             else if (sum < 0) {
+//                 i++;
+//             } 
+//             else {
+//                 j--;
+//             }
+//         }
+//         return ans;
+//     }
+// }
