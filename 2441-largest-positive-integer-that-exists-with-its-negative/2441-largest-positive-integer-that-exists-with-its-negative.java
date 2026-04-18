c67c2c4cf168b064982c2d0d643b9dda080a2c5c
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-18 22:12:22 +0530

    Time: 7 ms (58.64%), Space: 47 MB (70.79%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -5,13 +5,13 @@ class Solution {
         for(int num : nums){
             set.add(num);
         }
-        
         int ans = -1;
         for(int num : set){
             if(set.contains(-num)){
                 ans = Math.max(ans,num);
             }
         }
+        
         return ans;
     }
 }
