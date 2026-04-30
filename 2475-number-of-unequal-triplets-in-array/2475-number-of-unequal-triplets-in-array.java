45d21f92fa97fc001354eb03b368fc2877ccefe7
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-04-30 23:14:30 +0530

    Time: 2 ms (96.38%), Space: 43.5 MB (5.43%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
@@ -1,16 +1,38 @@
 class Solution {
     public int unequalTriplets(int[] nums) {
-        int n = nums.length, count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                for(int k = j+1; k < n; k++){
-                    if(nums[i] != nums[j] && nums[j] != nums[k] && nums[i] != nums[k]){
-                        count++;
-                    }
-                }
-            }
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return count;
+        int ans = 0;
+        int a = 0;
+
+        for (int b : map.values()) {
+            int c = n - a - b;
+            ans += a * b * c;
+            a += b;
+        }
+
+        return ans;
+        
     }
 }
+
+// class Solution {
+//     public int unequalTriplets(int[] nums) {
+//         int n = nums.length, count = 0;
+
+//         for(int i = 0; i < n; i++){
+//             for(int j = i+1; j < n; j++){
+//                 for(int k = j+1; k < n; k++){
+//                     if(nums[i] != nums[j] && nums[j] != nums[k] && nums[i] != nums[k]){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
