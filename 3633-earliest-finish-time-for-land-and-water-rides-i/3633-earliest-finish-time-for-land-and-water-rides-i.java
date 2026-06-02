bec10ac0151135500918d40dcda6884cc440a689
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-02 09:56:07 +0530

    Time: 1 ms (100%), Space: 47.1 MB (23.41%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,40 +1,35 @@
 class Solution {
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int n = landStartTime.length;
-        int m = waterStartTime.length;
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+            int[] waterStartTime, int[] waterDuration) {
+
         int finishTime = Integer.MAX_VALUE;
 
-        for(int i = 0; i < n; i++){
-            int time = landStartTime[i] + landDuration[i];
-
-            for(int j = 0; j < m; j++){
-                if(time < waterStartTime[j]){
-                    finishTime = Math.min(finishTime, waterStartTime[j] + waterDuration[j]);
-                }
-
-                else {
-                    finishTime = Math.min(finishTime, time + waterDuration[j]);
-                }
-            }
-
+        int minLandFinish = Integer.MAX_VALUE;
+        for (int i = 0; i < landStartTime.length; i++) {
+            minLandFinish = Math.min(
+                    minLandFinish,
+                    landStartTime[i] + landDuration[i]);
         }
 
-        for(int i = 0; i < m; i++){
-            int time = waterStartTime[i] + waterDuration[i];
+        for (int j = 0; j < waterStartTime.length; j++) {
+            finishTime = Math.min(
+                    finishTime,
+                    Math.max(minLandFinish, waterStartTime[j]) + waterDuration[j]);
+        }
 
-            for(int j = 0; j < n; j++){
-                if(time < landStartTime[j]){
-                    finishTime = Math.min(finishTime, landStartTime[j] + landDuration[j]);
-                }
+        int minWaterFinish = Integer.MAX_VALUE;
+        for (int i = 0; i < waterStartTime.length; i++) {
+            minWaterFinish = Math.min(
+                    minWaterFinish,
+                    waterStartTime[i] + waterDuration[i]);
+        }
 
-                else {
-                    finishTime = Math.min(finishTime, time + landDuration[j]);
-                }
-            }
-
+        for (int j = 0; j < landStartTime.length; j++) {
+            finishTime = Math.min(
+                    finishTime,
+                    Math.max(minWaterFinish, landStartTime[j]) + landDuration[j]);
         }
 
         return finishTime;
     }
-
 }
