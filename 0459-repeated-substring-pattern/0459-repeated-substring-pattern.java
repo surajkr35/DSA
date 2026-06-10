ccfb9f1f79d377e90e690d0c244b93825aa5457e
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-10 09:57:47 +0530

    Time: 34 ms (65.19%), Space: 46.9 MB (35.53%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        String temp = s + s;
-        String modified = temp.substring(1, temp.length() - 1);
-        return modified.contains(s);
+        int n = s.length();
+
+        for(int i = 1; i <= n / 2; i++){
+            if(n % i == 0){
+                String pattern = s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+
+                int times = n / i;
+                for(int j = 0; j < times; j++){
+                    sb.append(pattern);
+                }
+
+                if(sb.toString().equals(s)) return true;
+            }
+        }
+
+        return false;
     }
 }
+
 // class Solution {
 //     public boolean repeatedSubstringPattern(String s) {
-//         int n = s.length();
-
-//         for(int i = 1; i <= n / 2; i++){
-//             if(n % i == 0){
-//                 String pattern = s.substring(0, i);
-//                 StringBuilder sb = new StringBuilder();
-
-//                 int times = n / i;
-//                 for(int j = 0; j < times; j++){
-//                     sb.append(pattern);
-//                 }
-
-//                 if(sb.toString().equals(s)) return true;
-//             }
-//         }
-
-//         return false;
+//         String temp = s + s;
+//         String modified = temp.substring(1, temp.length() - 1);
+//         return modified.contains(s);
 //     }
 // }
