028a722e710b4556a6b6643bfbe9616a62da1f3c
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-06-11 18:02:05 +0530

    Time: 5 ms (78.83%), Space: 46.2 MB (6.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -15,19 +15,3 @@ class Solution {
     }
 }
 
-// class Solution {
-//     public boolean isPalindrome(int x) {
-//         if(x < 0) return false;
-//         int n = x;
-//         int rev = 0;
-
-//         while(n > 0){
-//             int r = n % 10;
-//             rev = rev * 10 + r;
-//             n = n / 10;
-//         }
-//         if(rev == x) return true;
-
-//         return false;
-//     }
-// }
