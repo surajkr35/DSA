761944c0dfd578119040d3b208b69a7d7742d402
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-08-11 23:39:31 +0530

    Time: 0 ms (100%), Space: 44 MB (77.96%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -21,10 +21,33 @@ class Solution {
                 }
             }
             if(!found){
-                    return sum;
-                }
-                sum++;
+                return sum;
+            }
+            sum++;
         }
-        
     }
 }
+// class Solution {
+//     public int missingInteger(int[] nums) {
+//         int n = nums.length;
+//         Set<Integer> set = new HashSet<>();
+//         int sum = nums[0];
+
+//         for(int num : nums){
+//             set.add(num);
+//         }
+//         for(int i = 0; i < n-1; i++){
+//             if(nums[i] + 1 == nums[i+1]){
+//                 sum += nums[i+1];
+//             }
+//             else {
+//                 break;
+//             }
+//         }
+//         while(set.contains(sum)){
+//             sum++;
+//         }
+
+//         return sum;
+//     }
+// }
