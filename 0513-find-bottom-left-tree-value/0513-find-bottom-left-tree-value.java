fb0249e08270ea39456e2aa6b94c78fffc67f410
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-08-20 00:17:57 +0530

    Time: 0 ms (100%), Space: 46.7 MB (19.24%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -1,27 +1,51 @@
+//  Using DFS
 class Solution {
+    int maxLevel = -1;
+    int ans = 0;
     public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        int ans = root.val;
+        value(root, 0);
 
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-
-            for(int i = 0; i < size; i++){
-                TreeNode temp = q.remove();
-                if(i == 0){
-                    ans = temp.val;
-                }
-
-                if(temp.left != null){
-                    q.add(temp.left);
-                }
-                if(temp.right != null){
-                    q.add(temp.right);
-                }
-            }
-        }
         return ans;
     }
+    private void value(TreeNode root, int level){
+        if(root == null) return;
+
+        if(level > maxLevel){
+            maxLevel = level;
+            ans = root.val;
+        }
+
+        value(root.left, level + 1);
+        value(root.right, level + 1);
+    }
 }
+
+//   Using BFS
+
+// class Solution {
+//     public int findBottomLeftValue(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         int ans = root.val;
+
+//         q.add(root);
+
+//         while(!q.isEmpty()){
+//             int size = q.size();
+
+//             for(int i = 0; i < size; i++){
+//                 TreeNode temp = q.remove();
+//                 if(i == 0){
+//                     ans = temp.val;
+//                 }
+
+//                 if(temp.left != null){
+//                     q.add(temp.left);
+//                 }
+//                 if(temp.right != null){
+//                     q.add(temp.right);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
