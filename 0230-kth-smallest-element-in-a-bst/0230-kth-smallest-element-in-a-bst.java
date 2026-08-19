8d0eac6ef332f91638ef5dfdd98ff542e15c2c32
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-08-20 01:03:42 +0530

    Time: 0 ms (100%), Space: 46.6 MB (69.53%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,23 @@
+class Solution {
+    int count = 0;
+    int ans = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+
+        return ans;
+    }
+
+    private void helper(TreeNode root, int k){
+        if(root == null) return;
+
+        helper(root.left, k);
+        
+        count++;
+
+        if(count == k){
+            ans = root.val;
+        }
+
+        helper(root.right, k);
+    }
+}
