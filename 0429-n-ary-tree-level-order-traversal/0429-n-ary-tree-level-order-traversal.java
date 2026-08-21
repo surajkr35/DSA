a3cbe645b3fdcb2a79df7f9f40f62a746515f9ce
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-08-21 12:13:47 +0530

    Time: 3 ms (93.1%), Space: 46.5 MB (74.38%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -1,54 +1,70 @@
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        Queue<Node> q = new LinkedList<>();
 
         if(root == null){
             return ans;
         }
+
+        Queue<Node> q = new LinkedList<>();
+        
         q.add(root);
-        q.add(null);
 
         while(!q.isEmpty()){
-            Node temp = q.remove();
+            int n = q.size();
+            List<Integer> list = new ArrayList<>();
 
-            if(temp == null){
-               ans.add(list);
-               list = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                Node temp = q.remove();
 
-               if(!q.isEmpty()){
-                q.add(null);
-               }
-            }
-            else {
                 list.add(temp.val);
+
                 for(Node child : temp.children){
                     if(child != null){
                         q.add(child);
                     }
                 }
             }
+            ans.add(list);
+            
         }
         return ans;
     }
 }
+// class Solution {
+//     public List<List<Integer>> levelOrder(Node root) {
+//         List<List<Integer>> ans = new ArrayList<>();
 
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
+//         if(root == null){
+//             return ans;
+//         }
 
-    public Node() {}
+//         List<Integer> list = new ArrayList<>();
+//         Queue<Node> q = new LinkedList<>();
+        
+//         q.add(root);
+//         q.add(null);
 
-    public Node(int _val) {
-        val = _val;
-    }
+//         while(!q.isEmpty()){
+//             Node temp = q.remove();
 
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
+//             if(temp == null){
+//                ans.add(list);
+//                list = new ArrayList<>();
+
+//                if(!q.isEmpty()){
+//                 q.add(null);
+//                }
+//             }
+//             else {
+//                 list.add(temp.val);
+//                 for(Node child : temp.children){
+//                     if(child != null){
+//                         q.add(child);
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
