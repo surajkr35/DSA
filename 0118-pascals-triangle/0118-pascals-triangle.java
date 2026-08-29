dd3f2f3a96a5bb6c5443763071c0205f8c9f1bf9
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-08-30 00:06:10 +0530

    Time: 1 ms (95.47%), Space: 43.7 MB (16.22%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,29 +1,50 @@
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
+//         list.add(1);
+//         ans.add(list);
+
+//         helper(1, numRows, list, ans);
+
+//         return ans;
+//     }
+
+//     private void helper(int k, int n, List<Integer> list, List<List<Integer>> ans){
+//         if(k == n){
+//             return;
+//         }
+//         List<Integer> temp = new ArrayList<>();
+//         temp.add(1);
+        
+//         for(int i = 0; i < list.size()-1; i++){
+//             int num = list.get(i) + list.get(i + 1);
+//             temp.add(num);
+//         }
+//         temp.add(1);
+//         ans.add(new ArrayList<>(temp));
+
+//         helper(k + 1, n, temp, ans);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        ans.add(list);
 
-        helper(1, numRows, list, ans);
-        
+        for(int i = 0; i < numRows; i++){
+            List<Integer> temp = new ArrayList<>();
+
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    temp.add(1);
+                }
+                else {
+                    temp.add(ans.get(i - 1).get(j-1) + ans.get(i - 1).get(j));
+                }
+            }
+            ans.add(temp);
+        }
         return ans;
-    }
-
-    private void helper(int k, int n, List<Integer> list, List<List<Integer>> ans){
-        if(k == n){
-            return;
-        }
-        List<Integer> temp = new ArrayList<>();
-        temp.add(1);
-        
-        for(int i = 0; i < list.size()-1; i++){
-            int num = list.get(i) + list.get(i + 1);
-            temp.add(num);
-        }
-        temp.add(1);
-        ans.add(new ArrayList<>(temp));
-
-        helper(k + 1, n, temp, ans);
     }
 }
