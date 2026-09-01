57b559cf11687664d44fb69545471a82107cb9f3
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-01 22:39:35 +0530

    Time: 46 ms (71.78%), Space: 74.1 MB (78.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -6,7 +6,6 @@ class Solution {
         for(int i = 0; i < k; i++){
             minHeap.add(nums[i]);
         }
-
         for(int i = k; i < n; i++){
             if(nums[i] > minHeap.peek()){
                 minHeap.poll();
