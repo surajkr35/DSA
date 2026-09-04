1b498a45e218418490ba59b6c18325b3e8609b0e
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-04 22:54:49 +0530

    Time: 2 ms (89.02%), Space: 47.6 MB (91.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int jump = Integer.MIN_VALUE;
+        int jump = 0;
 
         for(int i = 0; i < n; i++){
             if(nums[i] >= jump){
