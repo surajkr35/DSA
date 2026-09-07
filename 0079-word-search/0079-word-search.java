b00415f02eeeded0f33a2ed841240382b8d78c8c
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-08 00:07:49 +0530

    Time: 136 ms (51.78%), Space: 43.3 MB (17.88%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int r = board.length;
+        int c = board[0].length;
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(check(board, i, j, word, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean check(char[][] board, int r, int c, String s, int i){
+        if(i == s.length()){
+            return true;
+        }
+
+        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length || s.charAt(i) != board[r][c]){
+            return false;
+        }
+
+        char temp = board[r][c];
+        board[r][c] = 'X';
+
+        boolean res = check(board, r + 1, c, s, i + 1) ||
+                      check(board, r - 1, c, s, i + 1) ||
+                      check(board, r, c + 1, s, i + 1) ||
+                      check(board, r, c - 1, s, i + 1);
+
+        board[r][c] = temp;
+
+        return res;
+    }
+}
