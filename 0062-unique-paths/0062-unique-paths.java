5ebb250d8554ab43bbff7c4e402ba603995c9263
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-11 00:16:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,40 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int k = Math.min(m - 1, n - 1);
-        long result = 1;
-
-        for(int i = 1; i <= k; i++){
-            result = result * (m + n - 2 - k + i) / i;
+        int[][] temp = new int[m][n];
+        if(m == 1 || n == 1){
+            return 1;
         }
 
-        return (int) result;
+        temp[0][0] = 0;
+        for(int i = 1; i < m; i++){
+            temp[i][0] = 1;
+        }
+        for(int i = 1; i < n; i++){
+            temp[0][i] = 1;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                temp[i][j] = temp[i-1][j] + temp[i][j-1];
+            }
+        }
+        return temp[m-1][n-1];
     }
+    
 }
+// ____________________________________________________
+
+    // ~ Using the Permutation and Combinations (MATHS)
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int k = Math.min(m - 1, n - 1);
+//         long result = 1;
+
+//         for(int i = 1; i <= k; i++){
+//             result = result * (m + n - 2 - k + i) / i;
+//         }
+
+//         return (int) result;
+//     }
+// }
