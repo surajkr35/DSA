fb4ad0c5e24684990691164c8d637bff98d22ef5
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-11 21:37:13 +0530

    Time: 3 ms (96.42%), Space: 110.6 MB (88.72%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -1,19 +1,16 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = costs[0] + 1;
-        if(n > 1){
-            dp[2] = Math.min(dp[1] + 1, dp[0] + 4) + costs[1];
-        }
-        if(n > 2){
-            dp[3] = Math.min(dp[0] + 9, Math.min(dp[1] + 4, dp[2] + 1)) + costs[2];
+        int dp1 = 0;
+        int dp2 = 0;
+        int dp3 = 0;
+
+        for(int i = 1; i <= n; i++){
+            int curr = Math.min(dp1 + 9, Math.min(dp2 + 4, dp3 + 1)) + costs[i-1];
+            dp1 = dp2;
+            dp2 = dp3;
+            dp3 = curr;
         }
 
-        for(int i = 4; i <= n; i++){
-            dp[i] = Math.min(dp[i-1] + 1, Math.min(dp[i-2] + 4, dp[i-3] + 9)) + costs[i-1];
-        }
-        
-        return dp[n];
+        return dp3;
     }
 }
