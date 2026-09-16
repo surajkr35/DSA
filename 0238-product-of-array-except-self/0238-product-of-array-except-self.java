110b5a174ad2135e1aec944cf9f5d083eefcac16
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-16 10:01:45 +0530

    Time: 2 ms (92.97%), Space: 72 MB (55.53%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -25,10 +25,10 @@ class Solution {
         }
         else {
             for(int i = 0; i < n; i++){
+              
                 ans[i] = product / nums[i];
             }
         }
-
         return ans;
     }
 }
