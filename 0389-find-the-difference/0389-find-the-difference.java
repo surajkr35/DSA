64a96626b1e0f3400a8347fbb7e7b3c2fb3ab76b
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-18 11:14:31 +0530

    Time: 1 ms (99.75%), Space: 43.3 MB (19.7%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,23 +1,41 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int n = s.length();
-
-        int[] freq = new int[26];
-
-        for(int i = 0; i < n; i ++){
-            char ch = s.charAt(i);
-            freq[ch - 'a']++;
+        char ans = 0;
+        for(char ch : s.toCharArray()){
+            ans ^= ch;
         }
 
-        for(int i = 0; i < t.length(); i++){
-            char ch = t.charAt(i);
-            if(freq[ch - 'a'] == 0){
-                return ch;
-            }
-            else {
-                freq[ch - 'a']--;
-            }
+        for(char ch : t.toCharArray()){
+            ans ^= ch;
         }
-        return 'a';
+
+        return ans;
     }
 }
+// _________________________________________________
+
+    // ~ Using Freqency Array   ~  O(n) Time & O(26) ~ O(1) Space Complexity
+
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int n = s.length();
+
+//         int[] freq = new int[26];
+
+//         for(int i = 0; i < n; i ++){
+//             char ch = s.charAt(i);
+//             freq[ch - 'a']++;
+//         }
+
+//         for(int i = 0; i < t.length(); i++){
+//             char ch = t.charAt(i);
+//             if(freq[ch - 'a'] == 0){
+//                 return ch;
+//             }
+//             else {
+//                 freq[ch - 'a']--;
+//             }
+//         }
+//         return 'a';
+//     }
+// }
