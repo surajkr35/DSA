90241202eb81e3f415dbe25071e0890c8e6910c4
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-24 12:36:39 +0530

    Time: 1 ms (99.69%), Space: 45.8 MB (11.42%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -9,13 +9,13 @@ class Solution {
         }
         return -1;
     }
+
     private int sum(int n){
         int sum = 0;
         while(n > 0){
             sum += n % 10;
             n= n / 10;
-        }
-            
+        } 
         return sum;
     }
 }
