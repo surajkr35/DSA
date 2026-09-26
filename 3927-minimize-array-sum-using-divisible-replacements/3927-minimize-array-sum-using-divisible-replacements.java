64a252411724223520da3f62c80d0cc10bd8c62d
Suraj Kumar <surajkumarjnv35@gmail.com>
2026-09-26 21:54:59 +0530

    Time: 11 ms (99.17%), Space: 130.6 MB (43.33%) - LeetHub

---
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long minArraySum(int[] nums) {
+        int max = 0;
+        long sum = 0;
+
+        for(int num : nums){
+            if(num == 1) 
+            return nums.length;
+            if(num > max){
+                max = num;
+            }
+        }
+
+        int[] min = new int[max+1];
+
+        for(int num : nums) min[num] = num;
+
+        for(int num : nums){
+            if(min[num] < num){
+                continue;
+            }
+
+            for(int i = num;i <= max;i += num){
+                if(num < min[i]) min[i] = num;
+            }
+        }
+
+        for(int num : nums){
+            sum += min[num];
+        }
+
+        return sum;
+    }
+}
